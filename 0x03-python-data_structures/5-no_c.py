2d7cf9cecf9e6297fdd9c2898769a01c34f99b0e
SOME <sourbar2001@gmail.com>
2022-12-05 06:42:08 -0800

    task 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    my_string_list = list(my_string)
+    count = 0
+    for element in my_string_list:
+        if element == 'c' or element == 'C':
+            my_string_list[count]=""
+        count += 1
+    return "".join(my_string_list)
