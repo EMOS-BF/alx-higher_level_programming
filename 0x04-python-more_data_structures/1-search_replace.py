c293c2633dc85a4ced7c6624f210652da08f6855
SOME <sourbar2001@gmail.com>
2022-12-08 13:15:49 -0800

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    cpt = 0
-    for elt in my_list:
-        if elt == search:
-            my_list[cpt] = replace
-            cpt = cpt + 1
-        else:
-            cpt = cpt + 1
-    return my_list
+    return [find if find != search else replace for find in my_list]
