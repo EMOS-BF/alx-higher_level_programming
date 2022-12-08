18bce3598ee22b8bedafcd48aebcef8965802458
SOME <sourbar2001@gmail.com>
2022-12-08 13:53:18 -0800

    task 2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    res_list = []
+    sum = 0
+    for elt in my_list:
+        if elt not in res_list:
+            res_list.append(item)
+    for i in res_list:
+        sum += uniqs
+    return sum
