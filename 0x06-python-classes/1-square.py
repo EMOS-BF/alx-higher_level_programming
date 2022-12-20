bcf5828c81b78d69bdb11ca5f3ec7faffce0fd1c
SOME <sourbar2001@gmail.com>
2022-12-20 15:31:31 -0800

    task 0 task1 and Task 2

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """File defines a square"""
-
 class Square:
-"""A class Square that defines a square"""
+    """A class Square that defines a square"""
     def __init__(self, size=None):
-"""Initializes private attribute size """
+        """Initializes private attribute size """
         self.__size = size
