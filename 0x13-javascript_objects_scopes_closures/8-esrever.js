5c4d3246c95a9774b242e3867b85de12044a366f
SOME <sourbar2001@gmail.com>
2023-03-17 10:45:13 +0000

    task 8 correction

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   // function that returns the reversed version of a list
-  return list.reverse();
+  const reverselist = [];
+  for (let i = list.length - 1; i > 0; i--) {
+    reverselist.push(list[i]);
+  }
+  return reverselist;
 };
