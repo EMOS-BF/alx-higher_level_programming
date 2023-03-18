6605f0b60e435d0df7366718915f6d17bcdce7b0
SOME <sourbar2001@gmail.com>
2023-03-18 15:00:31 -0700

    task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,10 +3,10 @@
 from the database hbtn_0e_0_usa """
 if __name__ == '__main__':
 
-    import mysqldb
+    import MySQLdb
     import sys
 
-    db = mysqldb.connect(host="localhost", port=3306, user=argv[1],
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          password=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states\
