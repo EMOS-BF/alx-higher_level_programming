29f18b14d5351b1dc308dbf332a27d4646c0eabb
SOME <sourbar2001@gmail.com>
2023-03-19 15:25:08 -0700

    task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -14,11 +14,12 @@ if __name__ == "__main__":
 
     # create cursor to exec queries using SQL; join two tables for all info
     cur = db.cursor()
-    sql_cmd = """SELECT cities.id, cities.name, states.name
-                 FROM states
-                 INNER JOIN cities ON states.id = cities.state_id
-                 ORDER BY cities.id ASC"""
-    cur.execute(sql_cmd)
+    cur.execute("\
+    SELECT c.id, c.name, s.name FROM cities AS c\
+    JOIN states AS s\
+    ON s.id = c.state_id\
+    ORDER BY c.id ASC\
+    ")
     rows = cur.fetchall()
     for row in rows:
         print(row)
