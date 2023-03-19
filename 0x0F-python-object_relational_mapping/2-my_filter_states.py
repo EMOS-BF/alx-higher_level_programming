769e338cb88b50282f3d5570c3c083199ac12a6b
SOME <sourbar2001@gmail.com>
2023-03-19 03:51:09 -0700

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,7 +6,7 @@ hbtn_0e_0_usa where name matches the argument"""
 import MySQLdb
 from sys import argv
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2],
                          db=argv[3])
