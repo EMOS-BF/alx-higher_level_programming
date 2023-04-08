adab896844ce801976c3a1691502f2c0f2d094dd
SOME Barsour Evence <sourbar2001@gmail.com>
2023-04-08 15:43:56 +0200

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -9,14 +9,3 @@ def roman_to_int(roman_string):
         if elt in roman_dict.keys():
             somme = somme + roman_dict[elt]
     return somme
-
-roman_number = "X"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-roman_number = "VII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-roman_number = "IX"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-roman_number = "LXXXVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-roman_number = "DCCVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
