6e8f0b201bfb8487ea4c6e5884bab775c537be32
SOME <sourbar2001@gmail.com>
2023-05-12 21:25:53 +0000

    task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,7 +3,7 @@ const request = require('request');
 const fs = require('fs');
 
 request(process.argv[2], function (error, response, body) {
-  fs.writeFile(process.argv[3], body, 'utf8', function (err) {
+  fs.writeFile(process.argv[3], body, 'utf8', function (error) {
     if (error) {
       console.log(error);
     }
