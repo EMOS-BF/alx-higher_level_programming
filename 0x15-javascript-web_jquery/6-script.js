8c594a5954334bd2b85b2c31bdf4c75e41fa7890
SOME <sourbar2001@gmail.com>
2023-05-14 13:48:33 +0000

    Web jQuery

--- 0x15-javascript-web_jquery/6-script.js
+++ 0x15-javascript-web_jquery/6-script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $('DIV#add_item').click(function () {
-    $('UL.my_list').append('<li>Item</li>');
+  $('DIV#update_header').click(function () {
+    $('header').append('New Header!!!');
   });
 });
